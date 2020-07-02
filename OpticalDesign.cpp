eedc6a5a3b6745bbc5ab496b7869c94369de6b1b
Longqian Huang <longqianh@gmail.com>
2020-07-03 03:24:18 +0800

    Update OpticalDesign.cpp

--- OpticalDesign.cpp
+++ OpticalDesign.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <string.h>
+#include "Ray.h"
 #include "Lens.h"
 using namespace std;
+
+int Lens::len_num=1;
 
 class OptErr
 {
@@ -14,14 +17,11 @@ public:
 
 
 
-
-
 class OptSys
 {
 private:
 	vector<int> dist;
-	vector<Lens> lens;
-
+	// vector<Lens> lens;
 
 
 public:
@@ -33,37 +33,31 @@ public:
 		}
 	}
 
-	void init_lens(double paras[][2],int n){
-		for(int i=0;i<n;i++){
-			Lens len;
-			
-			lens.push_back(tmp);
-		}
+	// void init_lens(double paras[][2],int n){
+	// 	for(int i=0;i<n;i++){
+	// 		Lens len;
 
-	}
+	// 		lens.push_back(tmp);
+	// 	}
+
+	// }
 
 
 	void set_para(double *sys_paras,double len_paras[][2]){		
 		init_dist(sys_paras);
 		// init_lens()
 	}
-	void help(){
-		;
-	}
 
-
-	
-
-	int get_lens_num(){
-		return lens.size();
-	}
+	// int get_lens_num(){
+	// 	return lens.size();
+	// }
 
 	
 
-	Ray get_sysrayout(Ray rayin){
-		Ray sysrayout;
-		return sysrayout;
-	}
+	// Ray get_sysrayout(Ray rayin){
+	// 	Ray sysrayout;
+	// 	return sysrayout;
+	// }
 
 	void cal_opt_errs(){
 		;
@@ -81,27 +75,44 @@ public:
 
 int main()
 {
-	double U=10,l=-10;
+	// double U=10,l=-10;
 
 
-	double sys_paras[2]={1,10};// dist_num, dists
-	double len_paras[2][2];
+	// double sys_paras[2]={1,10};// dist_num, dists
+	// double len_paras[2][2];
 
-	Ray rayin;
-	rayin.set_para(U,l);
+	// Ray rayin;
+	// rayin.set_para(U,l);
 
-	OptSys optsys;
-	optsys.set_para(sys_paras,len_paras);
+	// // OptSys optsys;
+	// Lens len;
+	// Lens len2;
+	// cout<<Lens::len_cnt<<endl;
+	// optsys.set_para(sys_paras,len_paras);
 
 	// optsys.set_para(testdists,dist_num);
 	// Lens len1;
 
+	// Surface sf(5);
+	// double rho=sf.get_rho();
+	// double n=sf.get_n();
+	// cout<<rho<<' '<<n<<endl;
 
-	// rayin.help();
-	// rayin.ray_setting(20,-5,5.33);
-	// rayin.print_para();
 	// Lens len1;
-	// cout<<rayin.vd<<endl;
+	// Lens len2;
+	// len1.get_name();
+	// len2.get_name();
+	double rhos[]={1,4,2,3};
+	double nds[]={1.1,1.3,1.5,3};
+
+	// SingleLen sglen(rhos,nds,4);
+	// cout<<sglen.get_sfnum()<<endl;
+	// Ray myray(1,3);
+	// DoubleLen dblen(rhos,nds,4);
+	// cout<<dblen.get_sfnum()<<endl;
+	TriLen trilen(rhos,nds,4);
+	Surface sf1=trilen.get_sf(2);
+	cout<<sf1.get_rho()<<endl;
 	return 0;
 
 }
