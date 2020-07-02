3e0fb9bcb582b99d529091416e588dbdff1c3e35
Longqian Huang <longqianh@gmail.com>
2020-07-02 21:29:51 +0800

    Update OpticalDesign.cpp

--- OpticalDesign.cpp
+++ OpticalDesign.cpp
@@ -1,27 +1,77 @@
 #include<iostream>
+#include<vector>
+#include <string.h>
 #include "Lens.h"
 using namespace std;
+
+class OptErr
+{
+public:
+	OptErr();
+	~OptErr();
+		
+};
+
+
+
 
 
 class OptSys
 {
+private:
+	vector<int> dist;
+	vector<Lens> lens;
+
+
+
 public:
+
+	void init_dist(double * dists){
+		
+		for(int i=1;i<dists[int(dists[0])];i++){
+			dist.push_back(dists[i]);
+		}
+	}
+
+	void init_lens(double paras[][2],int n){
+		for(int i=0;i<n;i++){
+			Lens len;
+			
+			lens.push_back(tmp);
+		}
+
+	}
+
+
+	void set_para(double *sys_paras,double len_paras[][2]){		
+		init_dist(sys_paras);
+		// init_lens()
+	}
 	void help(){
 		;
 	}
-	void show_structure(){
-		;
-		// add optical structures in the form of matrix optics
-	}
 
-	void get_lens_num(){
-		;
+
+	
+
+	int get_lens_num(){
+		return lens.size();
 	}
 
 	
-	Ray get_sysrayout(){
+
+	Ray get_sysrayout(Ray rayin){
 		Ray sysrayout;
 		return sysrayout;
+	}
+
+	void cal_opt_errs(){
+		;
+	}
+
+	void show_structure(){
+		;
+		// add optical structures in the form of matrix optics
 	}
 
 };
@@ -29,12 +79,24 @@ public:
 
 
 
-
-
-
 int main()
 {
+	double U=10,l=-10;
+
+
+	double sys_paras[2]={1,10};// dist_num, dists
+	double len_paras[2][2];
+
 	Ray rayin;
+	rayin.set_para(U,l);
+
+	OptSys optsys;
+	optsys.set_para(sys_paras,len_paras);
+
+	// optsys.set_para(testdists,dist_num);
+	// Lens len1;
+
+
 	// rayin.help();
 	// rayin.ray_setting(20,-5,5.33);
 	// rayin.print_para();
