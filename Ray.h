85c1a3d1e5480c21f9154788745ce2a150cc28e2
Longqian Huang <longqianh@gmail.com>
2020-07-02 21:29:55 +0800

    Update Ray.h

--- Ray.h
+++ Ray.h
@@ -1,34 +1,37 @@
 #include<iostream>
 using namespace std;
 
+
 class Ray
 {
 private:
 
-	double vd;
 	double U;
 	double l;
+
+
 
 public:
 	Ray(){;}
 	~Ray(){;}
+
+	double i; // 入射角
+	double lh;
+
 	void help(){
 
-		cout<<"输入顺序: 物距、入射孔径角、阿贝常数"<<endl;
+		cout<<"输入顺序: 物距、入射孔径角"<<endl;
 
 	}
 
-	void ray_setting(double _U,double _l,double _vd){
-		vd=_vd;
+	void set_para(double _U,double _l){
 		U=_U;
 		l=_l;
-
 	}
 
 	void print_para(){
 		cout<<"物距: "<<l<<" mm"<<endl;
 		cout<<"入射孔径角: "<<U<< "度"<<endl;
-		cout<<"阿贝常数: "<<vd<<endl;
 	
 
 	}
