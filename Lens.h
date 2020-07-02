a7cc93059cc0fbdb6d8b46129c0f7e07958fa9b7
Longqian Huang <longqianh@gmail.com>
2020-07-02 21:29:46 +0800

    Update Lens.h

--- Lens.h
+++ Lens.h
@@ -8,21 +8,35 @@ class Lens
 {
 private:
 
-	double rho1; // radius of curvature
-	double rho2;
+	int sf_num;
+	double vd;
+	double nf,nd,nc;
+	// double rho1; // radius of curvature
+	// double rho2;
 	double t; // thick
 	double n; // 
 
+	struct Surface// surface
+	{
+		double rho;
+	};
+	
+	struct Surface sf[5];
+
 
 public:
-	Lens();
-	~Lens();
+	Lens(){;}
+	~Lens(){;}
 
 	void help(){
 
 	}
-	void set_paras(){
 
+	void set_paras(int sf_num,double *rho){
+		for(int i=0;i<sf_num;i++){
+			sf[i].rho=rho[i];	
+		}
+		
 	}
 
 	void print_paras(){
@@ -30,18 +44,20 @@ public:
 	}
 
 	double get_rho(int x){
-		if(x==1){
-			return this->rho1;
-		}
-		else{
-			return this->rho2;
-		}
+		// if(x==1){
+		// 	return rho1;
+		// }
+		// else{
+		// 	return rho2;
+		// }
+		return 0;
 	}
 
 
 	double get_thick(){
 		return t;
 	}
+
 
 	double get_n(){
 		// get refractive index
