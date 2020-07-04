a0ccc2c01ae7ad42fc28ab6d7a5d811345f50328
Haze <532587854@qq.com>
2020-07-04 16:32:19 +0800

    Create aboutdlg.cpp

---
+++ GUI/aboutdlg.cpp
@@ -0,0 +1,41 @@
+﻿// aboutdlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Ray_Tracing.h"
+#include "aboutdlg.h"
+#include "afxdialogex.h"
+
+
+// aboutdlg 对话框
+
+IMPLEMENT_DYNAMIC(aboutdlg, CDialogEx)
+
+aboutdlg::aboutdlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_ABOUTBOX, pParent)
+{
+
+}
+
+aboutdlg::~aboutdlg()
+{
+}
+
+void aboutdlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(aboutdlg, CDialogEx)
+	ON_COMMAND(ID_ABOUT, &aboutdlg::OnAbout)
+END_MESSAGE_MAP()
+
+
+// aboutdlg 消息处理程序
+
+void aboutdlg::OnAbout() 
+{
+	aboutdlg dlg;
+	dlg.DoModal();
+}
