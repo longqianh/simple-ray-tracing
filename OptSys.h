e9e7ef448e81b8a91c5a30d6f3030c1770f03265
Longqian Huang <longqianh@gmail.com>
2020-07-05 00:37:12 +0800

    Update OptSys.h

--- OptSys.h
+++ OptSys.h
@@ -8,7 +8,20 @@
 #ifndef INF
 #define INF 1e15
 #endif
-double ku=1,kw=1,flag=0;
+
+
+class OptAber
+{
+public:
+	OptAber();
+	~OptAber();
+	
+};
+
+
+
+
+
 
 class OptSys
 {
@@ -107,7 +120,7 @@ public:
 
 
 	// 利用多态
-	Ray ray_tracing(FPR rayin,bool isINF){
+	Ray ray_tracing(FPR rayin,bool isINF, double ku=1,double kw=1){
 		double u1,u2; // u1:u, u2:u'
 		double l1,l2;
 		double i;
@@ -116,22 +129,22 @@ public:
 		// cout<<"Rayin type: "<<label<<endl;
 		Ray rayout;
 
-		if(isINF) l1=-INF;
-		else l1=rayin.get_l();
+		if(isINF)
+		{
+			l1=-INF;
+			rayin.set_U(0);	
+		} 
+		else
+		{
+			l1=rayin.get_l();
+			u1=atan(a/l1);
+			rayin.set_U(u1);
+		}
+			
 		
-		u1=rayin.get_U();
-		
-		// cout<<Arc2Angle(u1)<<endl;
-
-		// cout<<u1<<" "<<sin(u1)<<endl;
-		// l1=-(a/2)/tan(u1);
-		// cout<<l1<<endl;// 需要加个判断 支持l和U
-		// rayin.set_l(l1);
 		for(int k=0;k<nsf;k++){
-			// cout<<"#"<<k;
-			double d=sf[k].get_d(); //cout<<"# "<<d<<endl;
+			double d=sf[k].get_d(); 
 			double rho=sf[k].get_rho();
-			// cout<<" "<<rho<<" "<<d<<" "<<endl;
 			
 			if(isINF&&k==0) i=(a/2)*rho;
 			else i=(rho*l1-1)*u1;				
@@ -142,17 +155,10 @@ public:
 			u2=(n2-n1)/n2*i+u1;
 
 			l2=(i+u1)/(u2*rho);
-			// cout<<" l1 "<<l1;
-			// cout<<" l2 "<<l2;
 			l1=l2-d;
-			n1=n2; // n1是n2前面的折射率
-
-			// cout<<" u1 "<<u1;
-			// cout<<" u2 "<<u2<<endl;
+			n1=n2; // n1是n2前面的折射率			
 			u1=u2;
-			// cout<<"i"<<i<<endl;
-			// cout<<"l1 "<<l1<<endl;
-				
+
 		}
 		
 		rayout.set_U(u2);
@@ -164,12 +170,13 @@ public:
 
 
 
-	Ray ray_tracing(SPR rayin,bool isINF){
+	Ray ray_tracing(SPR rayin,bool isINF,double ku=1,double kw=1){
 
 		double u1,u2;
 		double l1=0,l2;
 		double i;
-		double n2,n1=1;// n2:n', n1:n
+		double n2,n1=1;
+		 // n2:n', n1:n
 		// string label=rayin.get_label();
 		// cout<<"Rayin type: "<<label<<endl;
 		Ray rayout;
@@ -206,7 +213,6 @@ public:
 			l1=l2-d;
 			n1=n2; // n1是n2前面的折射率
 			u1=u2;
-
 		}
 
 		rayout.set_U(u2);
@@ -216,7 +222,7 @@ public:
 		return rayout;
 	}
 
-	Ray ray_tracing(FAR rayin,bool isINF){
+	Ray ray_tracing(FAR rayin,bool isINF,double ku=1,double kw=1){
 
 		double U1,U2; // U,U'
 		double l1,l2; // l,l'
@@ -225,10 +231,19 @@ public:
 		double d,rho;
 		Ray rayout;
 
-		if(isINF) l1=-INF;
-		else l1=rayin.get_l();
+		if(isINF)
+		{
+			l1=-INF;
+			rayin.set_U(0);
+		}
+
+		else
+		{
+			l1=rayin.get_l();
+			U1=asin(ku*sin(atan((a/2)/l1)));
+			rayin.set_U(U1);
+		} 
 		
-		U1=rayin.get_U();
 		
 		for(int k=0;k<nsf;k++){
 			d=sf[k].get_d(); 
@@ -264,9 +279,128 @@ public:
 	
 	}
 
+	Ray ray_tracing(SAR rayin,bool isINF,double ku=1,double kw=1){
 
-	void cal_opt_errs(){
-		;
+		double U1,U2; // U,U'
+		double l1,l2; // l,l'
+		double I1,I2; // I,I'
+		double n1=1,n2; // n,n'
+		double L; // 物距
+		double d,rho;
+		double s,t;
+		double y;
+		double *U2tmp= new double[nsf];
+
+		string label=rayin.get_label();
+
+		Ray rayout;
+
+
+		if(isINF){
+			double W=rayin.get_W();
+
+			U1=-kw*W; // 负号
+			t=-INF;
+			s=-INF;
+			if(label=="up"){
+				l1=ku*(a/2)/tan(U1);
+				// cout<<"UP"<<l1<<endl;
+			}
+			else if(label=="cf"){
+				l1=0;
+			}
+			else if(label=="dn")
+			{
+				l1=-ku*(a/2)/tan(U1);
+				// cout<<"DN"<<l1<<endl;	
+			}
+			rayin.set_l(l1);
+		} 
+
+		else{
+			L=rayin.get_l1();	
+			y=rayin.get_y();
+			t=-sqrt(y*y+L*L);
+			s=t;
+			if(label=="up")
+			{
+				U1=-atan((-y*kw+ku*a/2)/L);
+				l1=-ku*(a/2)/((-y*kw+ku*a/2)/L);
+			}
+			else if(label == "cf")
+			{
+				U1=-atan(-y*kw/L);
+				l1=0;
+			}
+			else if(label=="dn"){
+				cout<<"dn "<<(-y*kw-ku*a/2)/L<<endl;
+				U1=-atan((-y*kw-ku*a/2)/L);
+				l1=ku*(a/2)/((-y*kw-ku*a/2)/L);
+			}
+			rayin.set_U(U1);
+			rayin.set_l(l1);
+		} 
+		// cout<<"L1"<<l1<<endl;
+		if(label=="cf")
+		{
+			rayin.set_s(s);
+			rayin.set_t(t);
+		}
+		
+		
+		
+		for(int k=0;k<nsf;k++){
+			d=sf[k].get_d(); 
+			rho=sf[k].get_rho();
+			n2=sf[k].get_nd();
+
+			I1=asin((L*rho-1)*sin(U1));
+			I2=asin((n1/n2)*(L*rho-1)*sin(U1));
+			l2=1/rho*(1+sin(I2)/sin(U2));
+			U2=U1+I1-I2;
+			U2tmp[k]=U2;
+			l2=1/rho*(1+sin(I2)/sin(U2));
+			if(label=="cf"){
+				double PA=l1*sin(U1)/cos((I1-U1)/2);
+				cout<<" PA "<<PA<<endl;
+				sf[k].set_PA(PA);
+			}
+			
+			l1=l2-d;
+			U1=U2;
+			n1=n2; // n1是n2前面的折射率		
+		}
+		
+		rayout.set_U(U2);
+		rayout.set_l(l2);
+
+		if(label=="cf")
+		{
+			for(int k=0;k<nsf-1;k++)
+			{
+				d=sf[k].get_d();
+				double X1=sf[k].get_X();
+				double X2=sf[k+1].get_X();
+				double D=(d-X1+X2)/cos(U2tmp[k]);
+				t-=D;
+				s-=D;
+			}
+
+			rayout.set_t(t);
+			rayout.set_s(s);
+			
+		}
+		delete [] U2tmp;
+		
+		
+		return rayout;
+	
 	}
+
+	// OptAber cal_aber(SAR rayin_up,SAR rayin_cf,SAR rayin_dn)
+	// {
+	// 	OptAber abers;
+	// 	return abers;
+	// }
 
 };
