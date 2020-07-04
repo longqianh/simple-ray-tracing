b68b234d07ce943cbfb1da56c25d3c5ef5f5536f
Longqian Huang <longqianh@gmail.com>
2020-07-04 13:03:06 +0800

    Update Ray.h

--- Ray.h
+++ Ray.h
@@ -1,4 +1,6 @@
+#pragma once
 #include<iostream>
+#include "utils.h"
 using namespace std;
 
 
@@ -6,23 +8,25 @@ class Ray
 {
 protected:
 
-	double U;
+	double U; //弧度
+	double W; //弧度
 	double y;
-	double l;
-	double i; // 入射角
+	double l; // 物距
+	double i; // 入射角 弧度
+	string label;
 	// double lh;
 
 
 public:
-	Ray(){}
-	Ray(double U){
-		this->U=U;
-	}
+	Ray(){label="GR"; } // general ray
 	~Ray(){}
 
 
 	void set_U(double U){
 		this->U=U;
+	}
+	void set_W(double W){
+		this->W=W;
 	}
 	void set_l(double l){
 		this->l=l;
@@ -31,11 +35,18 @@ public:
 		this->i=i;
 	}
 
-
+	string get_label() const
+	{
+		return label;
+	}
 
 	double get_U() const
 	{
 		return U;
+	}
+	double get_W() const
+	{
+		return W;
 	}
 	double get_l() const
 	{
@@ -47,7 +58,8 @@ public:
 	}
 
 	void show_rayinfo(){
-		cout<<"U: "<<U<<endl;
+		cout<<"Ray type: "<<label<<endl;
+		cout<<"U: "<<Arc2Angle(U)<<endl;
 		cout<<"l: "<<l<<endl;
 	}
 	// void draw(){}
@@ -55,11 +67,50 @@ public:
 };
 
 
-class FAL: public Ray
+class FPL: public Ray
 {
+private:
+	double l1;
 public:
-	FAL(){}
-	FAL(double U): Ray(U) {}
-	~FAL(){}
+	FPL(){
+		label="FPL"; // First Paraxial Light
+	}
+	FPL(double U){
+		this->U=U;
+		label="FPL"; 
+	}
+	~FPL(){}
+
+
+};
+
+class SPL: public Ray
+{
+private:
+	double l1;
+	double W;
+
+public:
+	SPL(){
+		label="SPL"; // Second Paraxial Light
+		l=0;
+	}
+	SPL(double W){
+		label="SPL"; 
+		l=0;
+		this->W=W;
+	}
+	~SPL(){}
+
+	// a:入瞳直径
+	void set_l1(double a,double u){
+		l1=a/tan(u);
+	}
+
+	double get_l1() const
+	{
+		return l1;
 	
+	}
+
 };
